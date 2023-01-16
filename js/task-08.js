5f4f14e80529710e4d918b8e63098b9c4539fd46
HorbovyiY <yhorbovyi@gmail.com>
2023-01-16 21:04:07 +0200

    Add files via upload

--- js/task-08.js
+++ js/task-08.js
@@ -5,9 +5,17 @@ form.addEventListener("submit", onSubmit);
 function onSubmit(event) { 
     event.preventDefault();
 
+
     const { email, password } = event.currentTarget.elements;
-    console.log("email: ", email.value);
-    console.log("password: ", password.value);
+    // console.log("email: ", email.value);
+    // console.log("password: ", password.value);
+
+    const values={
+        email: email.value,
+        password: password.value,
+    }
+
+    console.log(values);
 
     if (email.value === '' || password.value === '') { 
         alert("Всі поля мають бути заповнені!");
